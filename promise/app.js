a6618b6bf23cc8271a477f9d8a01152c04eef440
albertshevchuk <albertshevchuk@gmail.com>
2021-02-01 11:52:01 -0800

    .then .catch Promise.all

--- promise/app.js
+++ promise/app.js
@@ -1,8 +1,26 @@
-const {checkInventory} = require('./library.js');
+const {checkInventory,checkAvailability} = require('./library.js');
 const order = [['sunglasses', 1], ['bags', 2]];
 
 const handleSuccess = (value) => {console.log(value);};
 
 const handleFailure = (value) => {console.log(value);};
 
+
+const onFulfill = (itemsArray) => {
+  console.log(`Items checked: ${itemsArray}`);
+  console.log(`Every item was available from the distributor. Placing order now.`);
+};
+
+const onReject = (rejectionReason) => {
+	console.log(rejectionReason);
+};
+
+const checkSunglasses = checkAvailability('sunglasses', 'Favorite Supply Co.');
+const checkPants = checkAvailability('pants', 'Favorite Supply Co.'); 
+const  checkBags = checkAvailability('bags', 'Favorite Supply Co.');
 checkInventory(order).then(handleSuccess, handleFailure);
+
+
+Promise.all([checkSunglasses, checkPants, checkBags])
+  .then(onFulfill)
+  .catch(onReject);
--- promise/library.js
+++ promise/library.js
@@ -4,6 +4,21 @@ const inventory = {
   bags: 1344
 };
 
+
+const checkAvailability = (itemName, distributorName) => {
+    console.log(`Checking availability of ${itemName} at ${distributorName}...`);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (restockSuccess()) {
+                console.log(`${itemName} are in stock at ${distributorName}`)
+                resolve(itemName);
+            } else {
+                reject(`Error: ${itemName} is unavailable from ${distributorName} at this time.`);
+            }
+        }, 1000);
+    });
+};
+
 const checkInventory = (order) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -17,4 +32,8 @@ const checkInventory = (order) => {
   })
 };
 
-module.exports = { checkInventory };
+function restockSuccess() {
+    return (Math.random() > .2);
+}
+
+module.exports = { checkInventory, checkAvailability };
